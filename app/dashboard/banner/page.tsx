b355b63aab8d3967466d81a7c73a45297702674c
AlefeVT <alefevt@gmail.com>
2024-08-07 08:44:46 -0300

    feat: form and list banner

---
+++ app/dashboard/banner/page.tsx
@@ -0,0 +1,83 @@
+import { getBanners } from "@/app/actions";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, User2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function BannerRoute() {
+
+    const data = await getBanners();
+    
+    return (
+        <>
+            <div className="flex items-center justify-end">
+                <Button asChild className="flex gap-x-2">
+                    <Link href={"/dashboard/banner/create"}>
+                        <PlusCircle className="h-3.5 w-3.5" />
+                        <span>Adicionar Banner</span>
+                    </Link>
+                </Button>
+            </div>
+
+            <Card className="mt-5">
+                <CardHeader>
+                    <CardTitle>
+                        Banners
+                    </CardTitle>
+                    <CardDescription>
+                        Gerencie os banners da sua loja
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Imagem</TableHead>
+                                <TableHead>Title</TableHead>
+                                <TableHead className="text-end">Ações</TableHead>
+                            </TableRow>
+                        </TableHeader>
+
+                        <TableBody>
+                            {data.map((item) => (
+                                <TableRow key={item.id}>
+                                <TableCell>
+                                    <Image 
+                                        alt="Imagem do Produto" 
+                                        src={item.imageString} 
+                                        width={64} 
+                                        height={64} 
+                                        className="rounded-lg object-cover h-16 w-16" 
+                                    />
+                                </TableCell>
+                                <TableCell className="font-medium">{item.title}</TableCell>
+                                <TableCell className="text-end">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button size={'icon'} variant={'ghost'}>
+                                                <MoreHorizontal className="h-4 w-4" />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem asChild>
+                                                <Link href={`/dashboard/banner/${item.id}/delete`}>
+                                                    Deletar
+                                                </Link>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
